ff2e112ce458595f3b9673e56c00f6bea1d36eeb
xiaoguo3606 <285726161@qq.com>
2017-07-12 19:11:52 +0800

    add content
    增加HTML与CSS内容

--- src/js/main.js
+++ src/js/main.js
@@ -8,8 +8,9 @@ $(document).ready(function(){
 
 function setIndexHeight(){
 	console.log(rem);
-	$('#header picture img').css('max-height',0.5*windowHeight);
+//	$('#header picture img').css('max-height',0.5*windowHeight);
 	$('#header').css('height',windowHeight);
+	$('.resume').css('margin-top',0);
 }
 
 //网站访问量计数器Ajax
