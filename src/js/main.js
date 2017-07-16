9562af1ef0ae5a7fcc8fa21f73467f4a454bf762
xiaoguo3606 <285726161@qq.com>
2017-07-16 16:24:27 +0800

    update files
    使用fullpage插件

--- src/js/main.js
+++ src/js/main.js
@@ -1,71 +1,96 @@
-//取出窗口的高度
-var windowHeight=$(window).innerHeight();
-var rem=parseInt($('html').css('font-size'));
 
 $(document).ready(function(){
-//	在窗口大小改变时,设置首页高度
-	$(window).resize(function(){
-		windowHeight = $(window).innerHeight();
-		$('body,html').css('height', windowHeight);
-		$('.content section').css('height',windowHeight);
-	});
 	
-	$(window).resize();
-	
-//	当窗口滚动时,监听滚动高度,当滚动高度大于100px时,显示回到顶部图标,否则隐藏
-	$(window).scroll(function(event){
-		var top=$(window).scrollTop();
-		if(top>100){
-			$('#arrow-to-top').show();
-		}else{
-			$('#arrow-to-top').hide();
-		}
-	});
-	$(window).scroll();
-	
-//	回到顶部
-	$('#arrow-to-top').click(function(){
-		scrollAnimate(0,500);
+//	启用fullpage相关预设
+	$('#content').fullpage({
+		menu:'#navbar-nav',
+		anchors:['header','resume','front-skill','other-skill','my-opus','contact-me'],
+//		设置导航栏的active及下一页按钮的显示与隐藏
+		onLeave:function(index,nextIndex,direction){
+//			由于导航栏中下拉菜单显示不正常,进行判断并解决
+			if(nextIndex==3 || nextIndex==4){
+				setTimeout(function(){
+					$('#dropdown').addClass('active');
+				},30);
+			}else{
+				setTimeout(function(){
+					$('#dropdown').removeClass('active');
+				},30);
+			}
+//			最后一页不再显示下一页按钮
+			if(nextIndex==6){
+				$('#arrow-to-next').hide();
+			}else{
+				$('#arrow-to-next').show();
+			}
+		},
+//		显示回到顶部按钮
+		afterLoad:function(anchorLink,index){
+			if(index>1){
+				$('#arrow-to-top').show();
+			}else{
+				$('#arrow-to-top').hide();
+			}
+		},
+//		触摸灵敏度,15为默认值
+		touchSensitivity: 15,
+//		在#resume-content中,滚动滚轮或滑动屏幕将不触发翻页
+		normalScrollElements:'#resume-content',
+//		垂直居中
+		verticalCentered:true,
+		//避免导航栏遮挡
+		paddingTop:'0.5rem',
+		
+		dragAndMove:true
 	});
 	
 //	下一页按钮
 	$('#arrow-to-next').click(function(){
-		var current=$('li.active').not('.dropdown').children('a').attr('href');
-		var authors=getAuthors();
-		var next=authors.indexOf(current)+1;
-		if(next < authors.length){
-			scrollAnimate($(authors[next]).offset().top,500);
-		}
+		$.fn.fullpage.moveSectionDown();
+	});
+//	回到顶端按钮
+	$('#arrow-to-top').click(function(){
+		$.fn.fullpage.moveTo('header');
 	});
 	
-	
-	
-//	为每一个导航添加点击滚动屏幕的事件
-	$('.navbar-nav li').each(function(index,ele){
-		var href=$(this).children('a').attr('href');
-//		导航栏中a标签必须有确切的值才添加点击事件
-		if(href && href!='#'){
-			$(this).click(function(event){
-				var temp=$(href).offset().top;
-				scrollAnimate(temp,500);
-				if($('.navbar-collapse').hasClass('in')){
-					$('.navbar-collapse').collapse('toggle');
-				}
-				return false;
-			});
-		}else{
-			return;
+//	移动端,点击各页面之后,收起导航栏
+	$('#navbar-nav li').not('.dropdown').on('click',function(){
+		if($('.navbar-collapse').hasClass('in')){
+			$('.navbar-collapse').collapse('toggle');
 		}
 	});
+
 	
-//	在导航栏打开时,点击空白处时收起导航栏
-	$('body').click(function(event){
+	
+	
+	
+	
+	
+//	针对移动端，在导航栏打开时,点击空白处时收起导航栏(点击除导航以外的区域)
+	$('body').children().not('header').click(function(event){
 		if($('.navbar-collapse').hasClass('in')){
 			$('.navbar-collapse').collapse('toggle');
 			return false;
 		}
 	});
+	
+	
+	
+//	设置title显示效果,基于jquery ui
+	$('#header li,#accordion a,sup').tooltip();
+	
+//	设置简历页中弹性标签效果,基于jquery ui
+	$('#accordion').accordion({
+		heightStyle:'fill'
+	});
+
 });
+
+
+
+
+
+
 
 /**
  * 自定义滚动窗口的动画
