118bb97b79ba5fb63cdf7d6b5390b7fecc54b8bb
xiaoguo3606 <285726161@qq.com>
2017-08-09 22:12:07 +0800

    更新内容

--- src/without-fullpage/js/main.js
+++ src/without-fullpage/js/main.js
@@ -20,6 +20,44 @@ $(document).ready(function(){
 			});
 		});
 	});
+	
+	setInterval(function(){
+		$('#today').text(function(){
+			var today=new Date(),
+				hourStr=today.getHours()>9 ? today.getHours().toString() : '0'+today.getHours(),
+				minutesStr=today.getMinutes()>9 ? today.getMinutes().toString() : '0'+today.getMinutes(),
+				secStr=today.getSeconds()>9 ? today.getSeconds().toString() : '0'+today.getSeconds();
+			return today.getFullYear()+'年'+(today.getMonth()+1)+'月'+today.getDate()+'日 '+hourStr+':'+minutesStr+':'+secStr;
+		});
+	},1000);
+	
+	$.ajax({
+		type:'POST',
+		async:false,
+		url:'https://api.weibo.com/oauth2/access_token',
+		data:{
+			client_id:'1496470922',
+			redirect_uri:'http://xiaoguo.pro'
+		},
+		success:function(data){
+			console.log(data)
+		},
+		error:function(){
+			console.log(error)
+		}
+	})
+	
+//	$.ajax({
+//		type:'GET',
+//		url:'https://api.weibo.com/2/common/get_country.json',
+//		success:function(data){
+//			console.log(data);
+//		},
+//		error:function(error){
+//			console.log(error)
+//		}
+//	})
+	
 });
 
 
